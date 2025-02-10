5638719af3405f04ae240af7052f51038db9ac55
Josy <160796147+JosielyAToledo@users.noreply.github.com>
2025-02-10 20:02:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,17 @@ const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
 
-
 const relogio = setInterval(function time() {
     let dateToday = new Date();
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
     let seg = dateToday.getSeconds();
 
-
-
-    if (hr < 10) hr = '0' + hr;
-    if (min < 10) min = '0' + min;
-    if (seg < 10) seg = '0' + seg;
+    if (hr < 10) hr = '0' + hr;  // Adiciona zero à esquerda se a hora for menor que 10
+    if (min < 10) min = '0' + min;  // Adiciona zero à esquerda se o minuto for menor que 10
+    if (seg < 10) seg = '0' + seg;  // Adiciona zero à esquerda se o segundo for menor que 10
 
     horas.textContent = hr;
     minutos.textContent = min;
     segundos.textContent = seg;
-})
+}, 1000);  // Atualiza a cada 1000 milissegundos (1 segundo)
